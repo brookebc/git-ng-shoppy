07dae78d7f39f932bd102d48a4af486c5c8aa1cc
brookebc <brooke.casey@gmail.com>
2014-05-23 02:32:15 -0400

    trying to clean up the mainjs so this will work

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -4,20 +4,17 @@ angular.module('shopNg')
 
 //this is for the blog
   .controller('PostsCtrl', function ($scope, $location, PostsSvc) {
-
-// <!-- create Post is used to save the content from the new blog post form-->
     $scope.createPost = function(post) {
     	$location.path('/new');
     };
-    // <!-- newPost is used to allow peeps to link to the spot to make a new post-->
     $scope.newPost = function() {
     	PostsSvc.create(post)
     	$location.path('/blog');
     };
     $scope.posts = PostsSvc.query();
   })
-  .controller('PostCtrl', function($scope, $location, $routeParams, PostSvc) {
 
+  .controller('PostCtrl', function($scope, $location, $routeParams, PostSvc) {
   	$scope.post = PostSvc.show({ id: $routeParams.id });
   	$scope.delete = function() { 
   		PostSvc.delete({ id: $routeParams.id });
@@ -30,7 +27,6 @@ angular.module('shopNg')
   })
   
   .controller('ShopCtrl', function ($scope, $location, ProductSvc) {
-
     $scope.createProduct = function() {
        ProductSvc.create(product)
       $location.path('/admin');
@@ -39,6 +35,6 @@ angular.module('shopNg')
       $location.path('/shop');
     };
     $scope.products = ProductSvc.query();
-  })
-
   });
+
+
