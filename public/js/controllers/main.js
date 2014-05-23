295ab48f98996acb981d18f8fe098f2c3d676c20
brookebc <brooke.casey@gmail.com>
2014-05-23 11:30:20 -0400

    updating this repo so that the create post will work

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,40 +1,68 @@
 'use strict';
 
 angular.module('shopNg')
-
-//this is for the blog
   .controller('PostsCtrl', function ($scope, $location, PostsSvc) {
+
     $scope.createPost = function() {
-    	$location.path('/new');
+      $location.path('/new');
     };
     $scope.newPost = function(post) {
-    	PostsSvc.create(post)
-    	$location.path('/blog');
+      PostsSvc.create(post)
+      $location.path('/blog');
     };
     $scope.posts = PostsSvc.query();
   })
-
   .controller('PostCtrl', function($scope, $location, $routeParams, PostSvc) {
-  	$scope.post = PostSvc.show({ id: $routeParams.id });
-  	$scope.delete = function() { 
-  		PostSvc.delete({ id: $routeParams.id });
-  		$location.path('/blog');
-  	};
-  	$scope.edit = function() {
-  		PostSvc.edit($scope.post);
-  		$location.path('/blog');
-  	};
+
+    $scope.post = PostSvc.show({ id: $routeParams.id });
+    $scope.delete = function() { 
+      PostSvc.delete({ id: $routeParams.id });
+      $location.path('/blog');
+    };
+    $scope.edit = function() {
+      PostSvc.edit($scope.post);
+      $location.path('/blog');
+    };
+
   });
-  //these are for the shop
-  // .controller('ShopCtrl', function ($scope, $location, ShopSvc) {
-  //   $scope.createProduct = function() {
-  //      ShopSvc.create(product)
-  //     $location.path('/admin');
-  //   };
-  //   $scope.newProduct = function(product) {
-  //     $location.path('/shop');
-  //   };
-  //   $scope.products = ShopSvc.query();
-  // });
+
+// 'use strict';
+
+// angular.module('shopNg')
+
+// //this is for the blog
+//   .controller('PostsCtrl', function ($scope, $location, PostsSvc) {
+//     $scope.createPost = function() {
+//     	$location.path('/new');
+//     };
+//     $scope.newPost = function(post) {
+//     	PostsSvc.create(post)
+//     	$location.path('/blog');
+//     };
+//     $scope.posts = PostsSvc.query();
+//   })
+
+//   .controller('PostCtrl', function($scope, $location, $routeParams, PostSvc) {
+//   	$scope.post = PostSvc.show({ id: $routeParams.id });
+//   	$scope.delete = function() { 
+//   		PostSvc.delete({ id: $routeParams.id });
+//   		$location.path('/blog');
+//   	};
+//   	$scope.edit = function() {
+//   		PostSvc.edit($scope.post);
+//   		$location.path('/blog');
+//   	};
+//   });
+//   //these are for the shop
+//   // .controller('ShopCtrl', function ($scope, $location, ShopSvc) {
+//   //   $scope.createProduct = function() {
+//   //      ShopSvc.create(product)
+//   //     $location.path('/admin');
+//   //   };
+//   //   $scope.newProduct = function(product) {
+//   //     $location.path('/shop');
+//   //   };
+//   //   $scope.products = ShopSvc.query();
+//   // });
 
 
--- public/js/services/services.js
+++ public/js/services/services.js
@@ -1,5 +1,6 @@
 'use strict';
 
+'use strict';
 angular.module('shopNg')
 	.factory('PostsSvc', function($resource) {
 		return $resource('api/collections/bestshopblog',
@@ -18,27 +19,49 @@ angular.module('shopNg')
 				show: { method: 'GET'},
 				edit: { method: 'PUT'},
 				delete: { method: 'DELETE'}
-			})
-
-	})
-	.factory('ShopSvc', function($resource) {
-		return $resource('api/collections/bestshop',
-			{},
-			{
-				query: { method: 'GET', isArray: true },
-				create: { method: 'POST'}
-			});
+			}
+			)
 	});
-	// .factory('ShopSvc', function($resource) {
-	// 	return $resource('api/collections/bestshop/:id',
-	// 		{
-	// 			id: '@_id'
-	// 		},
-	// 		{
-	// 			show: { method: 'GET'},
-	// 			edit: { method: 'PUT'},
-	// 			delete: { method: 'DELETE'}
-	// 		})
+
+// angular.module('shopNg')
+// 	.factory('PostsSvc', function($resource) {
+// 		return $resource('api/collections/bestshopblog',
+// 			{},
+// 			{
+// 				query: { method: 'GET', isArray: true },
+// 				create: { method: 'POST'}
+// 			});
+// 	})
+// 	.factory('PostSvc', function($resource) {
+// 		return $resource('api/collections/bestshopblog/:id',
+// 			{
+// 				id: '@_id'
+// 			},
+// 			{
+// 				show: { method: 'GET'},
+// 				edit: { method: 'PUT'},
+// 				delete: { method: 'DELETE'}
+// 			})
+
+// 	})
+// 	.factory('ShopSvc', function($resource) {
+// 		return $resource('api/collections/bestshop',
+// 			{},
+// 			{
+// 				query: { method: 'GET', isArray: true },
+// 				create: { method: 'POST'}
+// 			});
+// 	});
+// 	// .factory('ShopSvc', function($resource) {
+// 	// 	return $resource('api/collections/bestshop/:id',
+// 	// 		{
+// 	// 			id: '@_id'
+// 	// 		},
+// 	// 		{
+// 	// 			show: { method: 'GET'},
+// 	// 			edit: { method: 'PUT'},
+// 	// 			delete: { method: 'DELETE'}
+// 	// 		})
 		
-	// });
+// 	// });
 
